2f90c21eea4e8e5d3aeedc453321e71d7f2f7c87
Tobias <gitbender@gmx.com>
2022-11-14 18:05:05 +0100

    add avc factor for large financial sector entities to the corr_R function

--- R/corr_R.R
+++ R/corr_R.R
@@ -2,8 +2,12 @@
 #' coefficient of correlation R
 #'
 #' The coefficient of correlation R according to Sub-Section 2 Article 153 1. iii)
+#' for exposures to corporates, institutions and central governments and central banks
 #'
 #' @param PD enter the PD as a decimal number
+#' @param AVC enter the AVC factor to 1.15 for all exposures to large financial sector entities or to unregulated financial
+#' sector entities.
+#' default value is 1.
 #'
 #' @return  A Number
 #' @export
@@ -13,9 +17,12 @@
 #' PD <- 0.002
 #' corr_R(PD)
 #'
+#' #exposure to large financial sector entities
+#' PD <- 0.002
+#' corr_R(PD, AVC=1.15)
 #'
-corr_R <- function (PD){
+corr_R <- function (PD, AVC = 1){
   stopifnot("PD input must be numeric" = is.numeric(PD))
-  0.12 * (1 - exp(-50*PD))+0.24*(1-(1-exp(-50*PD))/(1-exp(-50)))
+  AVC * (0.12 * (1 - exp(-50*PD))+0.24*(1-(1-exp(-50*PD))/(1-exp(-50))))
 }
 
--- tests/testthat/test-corr_R.R
+++ tests/testthat/test-corr_R.R
@@ -2,3 +2,6 @@ test_that("check expected value R factor, non financial, non KMU", {
   expect_equal(round(corr_R(0.15),7), 0.1200664)
 })
 
+test_that("check expected value R factor, financial, non KMU", {
+  expect_equal(round(corr_R(0.15, AVC =1.25),7), 0.150083)
+})
